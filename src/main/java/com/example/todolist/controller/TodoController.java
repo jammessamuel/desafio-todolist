b620e8928282488af49757d0dd38499732544363
Samuel Jammes <samueljammes@MacBook-Air-de-Samuel.local>
2024-10-23 19:53:54 -0300

    todolist

---
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -0,0 +1,39 @@
+package com.example.todolist.controller;
+
+import com.example.todolist.entity.Todo;
+import com.example.todolist.service.TodoService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create( @RequestBody @Valid Todo todo) {
+        return todoService.create(todo);
+
+    }
+
+    @GetMapping
+    List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete( @PathVariable("id") Long id) {
+        return todoService.delete(id);
+    }
+}
---
+++ src/test/java/com/example/todolist/TodolistApplicationTests.java
@@ -0,0 +1,44 @@
+package com.example.todolist;
+
+import com.example.todolist.entity.Todo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TodolistApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	void testCreatedTodoSuccess () {
+		var todo = new Todo("todo 1", "desc todo 1", false, 1);
+
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(todo)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.lenght()").isEqualTo(1)
+				.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+				.jsonPath("$[0].descrição").isEqualTo(todo.getDescricao())
+				.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+				.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+	}
+
+	@Test
+	void testCreatedTodoFailure () {
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(
+						new Todo("","",false,0))
+				.exchange()
+				.expectStatus().isBadRequest();
+	}
+
+}
